aeaa1edaadbc68a4a0b57cda97296b06f79cc313
ElderJames <shunjiey@hotmail.com>
2018-04-13 01:48:25 +0800

    feat(): 增加测试用例

--- test/DNIC.Erechtheion.Tests/Repository/TopicRepositoryTest.cs
+++ test/DNIC.Erechtheion.Tests/Repository/TopicRepositoryTest.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Security.Claims;
 using DNIC.Erechtheion.Domain.Topic;
 using DNIC.Erechtheion.EntityFrameworkCore;
@@ -15,12 +16,85 @@ namespace DNIC.Erechtheion.Tests.Repository
 	/// </summary>
 	public class TopicRepositoryTest
 	{
-		[Fact(DisplayName = "Add_Topic_To_Database_Test")]
-		public void Add_Topic_To_Database_Test()
+		[Fact(DisplayName = "TopicRepository_Add_Test")]
+		public async void TopicRepository_Add_Test()
 		{
 			// Arrange
+			var topicRepository = GetReopsitory();
+			var topic = new Topic("erechtheion");
+
+			// Act
+			var addedTopic = await topicRepository.Create(topic);
+
+			// Assert
+			Assert.Equal("erechtheion", addedTopic.Name);
+			Assert.Equal(1, addedTopic.CreatorUserId);
+			Assert.True(addedTopic.Id > 0);
+		}
+
+		[Fact(DisplayName = "TopicRepository_Update_Test")]
+		public async void TopicRepository_Update_Test()
+		{
+			// Arrange
+			var topicRepository = GetReopsitory();
+			var topic = new Topic("erechtheion");
+
+			// Act
+			var addedTopic = await topicRepository.Create(topic);
+			addedTopic.Change("erechtheion_updated");
+			var updatedTopic = await topicRepository.Update(addedTopic);
+
+			// Assert
+			Assert.True(updatedTopic);
+			Assert.Equal("erechtheion_updated", addedTopic.Name);
+			Assert.Equal(1, addedTopic.CreatorUserId);
+			Assert.True(topic.LastModificationTime != null);
+		}
+
+		[Fact(DisplayName = "TopicRepository_GetById_Test")]
+		public async void TopicRepository_GetById_Test()
+		{
+			// Arrange
+			var topicRepository = GetReopsitory();
+			var topic = new Topic("erechtheion");
+			var addedTopic = await topicRepository.Create(topic);
+
+			// Act
+			var gotTopic = await topicRepository.GetById(addedTopic.Id);
+
+			// Assert
+			Assert.True(addedTopic.Id > 0);
+			Assert.NotNull(gotTopic);
+			Assert.Equal(gotTopic.Id, addedTopic.Id);
+			Assert.Equal("erechtheion", gotTopic.Name);
+			Assert.Equal(1, addedTopic.CreatorUserId);
+		}
+
+		[Fact(DisplayName = "TopicRepository_GetAll_Test")]
+		public async void TopicRepository_GetAll_Test()
+		{
+			// Arrange
+			var topicRepository = GetReopsitory();
+			var topic = new Topic("erechtheion");
+			var topic2 = new Topic("erechtheion2");
+
+			await topicRepository.Create(topic);
+			await topicRepository.Create(topic2);
+
+			// Act
+			var list = await topicRepository.GetAll();
+
+			// Assert
+
+			Assert.Contains(list, x => x.Name == "erechtheion");
+
+			Assert.Equal(2, list.Count());
+		}
+
+		private ITopicRepository GetReopsitory()
+		{
 			var options = new DbContextOptionsBuilder<ErechtheionDbContext>()
-				.UseInMemoryDatabase("Add_Topic_To_Database")
+				.UseInMemoryDatabase("Add_Topic_To_Database2")
 				.Options;
 
 			var claimsPrincipal = new ClaimsPrincipal(new List<ClaimsIdentity>()
@@ -32,19 +106,9 @@ namespace DNIC.Erechtheion.Tests.Repository
 			mock.Setup(acr => acr.HttpContext.User).Returns(claimsPrincipal);
 			var accessor = mock.Object;
 
-			var topic = new Topic("erechtheion");
-
 			var context = new ErechtheionDbContext(options, accessor);
 
-			ITopicRepository topicRepository = new TopicRepository(context);
-
-			// Act
-			var addedTopic = topicRepository.Create(topic).Result;
-
-			// Assert
-			Assert.Equal(addedTopic.Name, topic.Name);
-			Assert.Equal(1, addedTopic.CreatorUserId);
-			Assert.True(addedTopic.Id > 0);
+			return new TopicRepository(context);
 		}
 	}
 }
