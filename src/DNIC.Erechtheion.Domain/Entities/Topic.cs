b725290be9b6f8bac7da8066c3a14bd546062e74
Lewis <zlzforever@163.com>
2018-04-24 21:56:12 +0800

    add missing refenrence

--- src/DNIC.Erechtheion.Domain/Entities/Topic.cs
+++ src/DNIC.Erechtheion.Domain/Entities/Topic.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using DNIC.Erechtheion.Application.EnumTypes;
 using DNIC.Erechtheion.Core.Domain;
 using DNIC.Erechtheion.Domain.ValueObjects;
