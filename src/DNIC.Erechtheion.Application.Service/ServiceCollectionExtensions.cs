28992ff0ffaf7952d39dd3ee547ffadc6a137e80
ElderJames <shunjiey@hotmail.com>
2018-05-08 16:17:56 +0800

    feat(automapper): automapper配置自动注册

--- src/DNIC.Erechtheion.Application.Service/ServiceCollectionExtensions.cs
+++ src/DNIC.Erechtheion.Application.Service/ServiceCollectionExtensions.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using DNIC.Erechtheion.Core;
+using DNIC.Erechtheion.Core.Extensions;
+using DNIC.Erechtheion.Domain.Repositories;
+using DNIC.Erechtheion.SmartSql.Repositories;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DNIC.Erechtheion.Application.Service
@@ -7,17 +10,15 @@ namespace DNIC.Erechtheion.Application.Service
 	public static class ServiceCollectionExtensions
 	{
 		/// <summary>
-		/// 注册仓储等服务所需组件
+		/// 注册应用服务所需组件
 		/// </summary>
 		/// <param name="services"></param>
+		/// <param name="builderAction"></param>
 		public static void AddErechtheionServices(this IServiceCollection services, Action<IErechtheionBuilder> builderAction)
 		{
+			AutoMapperExtensions.Initialize();
 			var builder = new ErechtheionBuilder(services);
-
 			builderAction(builder);
-
-			//配置 TODO:分离后写在服务的Starup.cs
-			AutoMapperConfiguration.CreateMap();
 		}
 	}
 }
---
+++ src/DNIC.Erechtheion.Core/Extensions/AutoMapperExtensions.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using AutoMapper;
+
+namespace DNIC.Erechtheion.Core.Extensions
+{
+	public static class AutoMapperExtensions
+	{
+		private static bool _initialized = false;
+		private static readonly object Locker = new object();
+
+		static AutoMapperExtensions()
+		{
+			Initialize();
+		}
+
+		public static void Initialize()
+		{
+			if (!_initialized)
+			{
+				lock (Locker)
+				{
+					if (!_initialized)
+					{
+						Mapper.Initialize(cfg =>
+						{
+							cfg.ForAllMaps((typeMap, mappingExpr) =>
+							{
+								var ignoredPropMaps = typeMap.GetPropertyMaps();
+								foreach (var map in ignoredPropMaps)
+								{
+									var sourcePropInfo = map.SourceMember as PropertyInfo;
+									if (sourcePropInfo == null) continue;
+
+									if (sourcePropInfo.PropertyType != map.DestinationPropertyType)
+										map.Ignored = true;
+								}
+								mappingExpr.ForAllMembers(opt =>
+								{
+									if (ignoredPropMaps.All(p => opt.DestinationMember.Name != p.SourceMember.Name))
+									{
+										opt.Ignore();
+									}
+								});
+							});
+						});
+						_initialized = true;
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// 对象到对象
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="obj"></param>
+		/// <returns></returns>
+		public static T MapTo<T>(this object obj)
+		{
+			if (obj == null) return default(T);
+
+			return Mapper.Map<T>(obj);
+		}
+
+		/// <summary>
+		/// 集合到集合
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="obj"></param>
+		/// <returns></returns>
+		public static List<T> MapTo<T>(this IEnumerable obj)
+		{
+			if (obj == null) throw new ArgumentNullException();
+
+			return Mapper.Map<List<T>>(obj);
+		}
+	}
+}
